ef75a4ab433a57f8b71bf144f2c155d9f4102b4f
Kornkanok <67050665@kmitl.ac.th>
2026-09-23 00:26:21 +0700

    Add cell05 ex01play_with_arrays.py

---
+++ discovery_piscine/cell05/ex01/play_with_arrays.py
@@ -0,0 +1,6 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+for i in original_array :
+  new_array.append(i + 2)
+print(f"Original array: {original_array}")
+print(f"New array: {new_array}")
