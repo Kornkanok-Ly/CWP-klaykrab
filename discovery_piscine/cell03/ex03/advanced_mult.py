f42adac7f3c0c11121204144cc20976f72f0764d
Kornkanok <67050665@kmitl.ac.th>
2026-09-22 18:09:29 +0700

    Update advanced_mult.py

--- discovery_piscine/cell03/ex03/advanced_mult.py
+++ discovery_piscine/cell03/ex03/advanced_mult.py
@@ -1,19 +1,13 @@
 import sys
-
-# ตรวจสอบว่ามีการพิมพ์ argument ต่อท้ายตอนรันคำสั่งหรือไม่
 if len(sys.argv) > 1:
     print("none")
 else:
     i = 0
-    # while loop ตัวที่ 1: ควบคุมแม่สูตรคูณตั้งแต่แม่ 0 ถึง 10
     while i <= 10:
         print(f"Table de {i}:", end="")
-        
         j = 0
-        # while loop ตัวที่ 2: ควบคุมตัวคูณตั้งแต่คูณ 0 ถึง 10
         while j <= 10:
             print(f" {i * j}", end="")
             j += 1
-            
-        print() # ขึ้นบรรทัดใหม่เมื่อแสดงผลสูตรคูณแต่ละแม่เสร็จ
+        print() 
         i += 1
