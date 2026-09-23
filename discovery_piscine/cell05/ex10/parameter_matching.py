a047132c2ded49da0415a0aaed62788495d70c8d
Kornkanok <67050665@kmitl.ac.th>
2026-09-23 23:46:22 +0700

    Add cell05 ex10parameter_matching.py

---
+++ discovery_piscine/cell05/ex10/parameter_matching.py
@@ -0,0 +1,9 @@
+import sys
+if len(sys.argv) == 2:
+    user_word = input("What was the parameter? ")
+    if user_word == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+else:
+    print("none")
